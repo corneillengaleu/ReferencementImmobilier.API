168cf1d135e9e1a72a3fbc53861b14774fadfa2a
corneille <corneillengaleu@gmail.com>
2025-05-27 22:37:15 +0100

    Initial commit

---
+++ src/main/java/org/projectTutore/refencementImmobilier/RefencementImmobilierApplication.java
@@ -0,0 +1,13 @@
+package org.projectTutore.refencementImmobilier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RefencementImmobilierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RefencementImmobilierApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/projectTutore/refencementImmobilier/RefencementImmobilierApplicationTests.java
@@ -0,0 +1,13 @@
+package org.projectTutore.refencementImmobilier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RefencementImmobilierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
